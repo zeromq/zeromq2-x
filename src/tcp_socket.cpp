b69aee1b5e5aee9bdcab75aa1b5af88bfc7b0339
Pieter Hintjens <ph@imatix.com>
2011-07-18 13:39:51 +0200

    Avoid SIGPIPE errors on FreeBSD

--- src/tcp_socket.cpp
+++ src/tcp_socket.cpp
@@ -162,7 +162,7 @@ int zmq::tcp_socket_t::open (fd_t fd_, uint64_t sndbuf_, uint64_t rcvbuf_)
         errno_assert (rc == 0);
     }
 
-#ifdef ZMQ_HAVE_OSX || defined ZMQ_HAVE_FREEBSD
+#if defined ZMQ_HAVE_OSX || defined ZMQ_HAVE_FREEBSD
     int set = 1;
     int rc = setsockopt (s, SOL_SOCKET, SO_NOSIGPIPE, &set, sizeof (int));
     errno_assert (rc == 0);
