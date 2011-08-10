98b96e7d8fd60605ad1498cd540b07dd7040f168
Pieter Hintjens <ph@imatix.com>
2011-08-10 17:51:34 +0200

    Whitespace and comment changes

--- builds/android/clean.sh
+++ builds/android/clean.sh
@@ -1,21 +1,26 @@
+#! /bin/bash
+#
+#   Restores the libzmq tree after a build.sh. Does no harm if
+#   used more than once. Run from the builds/android directory.
+
 
 SRC_DIR="../../src"
 JNI_DIR="$SRC_DIR/jni"
 
 if [ -d $JNI_DIR ]; then
-	rm -rf $JNI_DIR;
+    rm -rf $JNI_DIR
 fi
 
 if [ -d $SRC_DIR/obj ]; then
-	rm -rf $JNI_DIR/obj
+    rm -rf $JNI_DIR/obj
 fi
 
 if [ -d $SRC_DIR/libs ]; then
-	rm -rf $SRC_DIR/libs;
+    rm -rf $SRC_DIR/libs
 fi
 
 if [ -h libzmq.so ]; then
-	rm libzmq.so;
+    rm libzmq.so
 fi
 
-exit 0;
+exit 0
